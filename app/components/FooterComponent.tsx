c59831568ab7854bbabf0b47b2d4dd639fdcaf40
Shubham Singh <coolsks94@gmail.com>
2024-01-14 21:29:08 +0530

    Design home page

--- app/components/FooterComponent.tsx
+++ app/components/FooterComponent.tsx
@@ -3,10 +3,10 @@ import { Link } from "@nextui-org/react";
 
 export default function FooterComponent() {
   return (
-    <footer className="absolute bottom-0 z-40 flex justify-center p-2 items-center w-full h-auto border-t border-divider">
-      <p className="text-center text-sm font-regular text-inherit">
+    <footer className="relative bottom-0 left-0 z-40 flex justify-center p-2 items-center w-full h-auto border-t border-divider">
+      <p className="text-center text-sm font-regular text-foreground-500">
         Designed with ❤️ by{" "}
-        <Link underline="hover" href="/">
+        <Link className={"text-primary-500"} underline="hover" href="/">
           Shubham Singh
         </Link>{" "}
         © {new Date().getFullYear()}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,65 @@
+import { Avatar, Divider } from "@nextui-org/react";
 import Image from "next/image";
+import {SiAndroidstudio, SiKotlin, SiReact, SiGit} from "react-icons/si";
+
+const skills = [
+  {
+    name: "Android",
+    icon: <SiAndroidstudio className="w-6 h-6 text-green-600" />,
+  },
+  {
+    name: "Kotlin",
+    icon: <SiKotlin className="w-6 h-6 text-fuchsia-600" />,
+  },
+  {
+    name: "ReactJs",
+    icon: <SiReact className="w-6 h-6 text-sky-400"/>,
+  },
+  {
+    name: "Git",
+    icon: <SiGit className="w-6 h-6 text-slate-500" />,
+  },
+];
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col min-h-screen justify-center items-center">
+      <section className="flex flex-row justify-between items-center gap-16 p-2 m-24 w-full max-sm:flex-col">
+        <div className="flex flex-col max-w-fit max-h-fit gap-4 m-auto max-sm:mx-24 items-start">
+          <p className="font-regular text-5xl uppercase text-foreground-500">
+            Hello,
+          </p>
+          <div className="flex flex-row items-start max-w-fit max-h-fit gap-2 max-sm:flex-col">
+            <p className="text-5xl font-regular text-foreground-500">
+              I&apos;M
+            </p>
+            <p className="text-5xl font-bold uppercase text-foreground whitespace-nowrap max-sm:whitespace-normal">
+              Shubham Singh
+            </p>
+          </div>
+          <p className="text-lg m-auto font-medium text-foreground-500">
+            I&apos;m a Software Engineer who loves making modern Android & Web apps.
+          </p>
+          <div className="flex flex-row gap-8">
+            {skills.map((skill, index) => (
+              <div className="flex flex-row gap-2 item-center justify-center" key={`${skill}+${index}`}>
+                {skill.icon}
+                <p className="text-md font-medium text-foreground">{skill.name}</p>
+              </div>
+              ))}
+          </div>  
+        </div>
+
+        <div className="flex max-w-fit flex-row m-auto items-star">
+          <Image
+            src={"/shubham-singh-dp-1.png"}
+            alt="Shubham Singh"
+            width={200}
+            height={200}
+            className="rounded-full border-spacing-16 border-4 border-foreground-400"
+          />
+        </div>
+      </section>
+    </main>
+  );
 }
