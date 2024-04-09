49adf9f124868af1991b2db753639282303221f2
Shubham Singh <coolsks94@gmail.com>
2024-04-09 10:54:58 +0530

    Update blog grid width

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -43,7 +43,8 @@ export default function BlogPage() {
     }, [searchQuery]);
 
     return (
-       <section className="flex p-8 mx-auto flex-col min-h-screen">
+       <section className="flex p-8 mx-auto flex-col min-h-screen items-center">
+        <div className='w-full md:w-5/6 lg:w-4/6 xl:3/6'>
         <div className='flex flex-col gap-8 mb-24 items-center justify-start md:flex-row md:justify-between sm:flex-row sm:justify-between'>
         <p className="text-4xl font-bold text-start">Blogs</p>
          <Input 
@@ -57,14 +58,14 @@ export default function BlogPage() {
          isClearable
          className='max-w-unit-6xl'/> 
         </div>
-       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8'>
+       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-8'>
           {searchQuery !== "" && data.length === 0 && 
-            <div className="flex w-full col-span-1 lg:col-span-3 md:col-span-2 items-center justify-center">
+            <div className="flex w-full col-span-1 lg:col-span-2 md:col-span-2 xl:col-span-3 items-center justify-center">
                 <p className='text-xl font-medium'>No results found</p>
             </div>
             }
             {isLoading && 
-             <div className="flex w-full col-span-1 lg:col-span-3 md:col-span-2 items-center justify-center">
+             <div className="flex w-full col-span-1 lg:col-span-2 md:col-span-2 xl:col-span-3 items-center justify-center">
              <CircularProgress size="md" aria-label="Loading..."/>
              </div>
             }
@@ -81,6 +82,7 @@ export default function BlogPage() {
               />
           ))}
         </div> 
+        </div>
         </section>
     )
 }
