dcf4bb887eea8c29b5338b5a232976579abff9cf
Shubham Singh <coolsks94@gmail.com>
2025-02-27 01:09:20 +0530

    chore: Move project data to separate file

--- app/components/ProjectComponent.tsx
+++ app/components/ProjectComponent.tsx
@@ -10,51 +10,7 @@ import {
   Link
 } from "@nextui-org/react";
 import Image from "next/image";
-
-const projects = [
-  {
-    name: "Splashy",
-    description: "An Unsplash based wallpaper app built with Compose Multiplatform and KMM for Android and iOS.",
-    techStack: ["Kotlin", "KMP", "Ktor", "Compose", "Voyager", "Koin", "SQLDelight"],
-    link: "https://github.com/ishubhamsingh/Splashy",
-    image: "https://raw.githubusercontent.com/ishubhamsingh/Splashy/main/assets/icon.png"
-  },
-  {
-    name: "AospExtended",
-    description: "An android custom ROM based on AOSP, which provides a stock experience along with customization features.",
-    techStack: ["Aosp", "Java", "Xml", "C++", "Make"],
-    link: "https://github.com/AospExtended",
-    image: "https://github.com/AospExtended.png"
-  },
-  {
-    name: "Omnitracs Mobile Manager",
-    description: "Omnitracs Mobile Manager is a mobile application that seamlessly integrates with your back-office Omnitracs web application and gives you the key information you need to manage your mobile resources while you’re on the go. You’ll have visibility into the day’s routes, orders, equipment and worker performance at your fingertips.",
-    techStack: ["Kotlin", "KMP", "Compose", "Ktor", "Hilt", "SQLDelight"],
-    link: "https://play.google.com/store/apps/details?id=com.roadnet.mobile.fleetview.android",
-    image: "/images/omm.webp"
-  },
-  {
-    name: "LoJack",
-    description: "LoJack helps keep you and your loved ones safe behind the wheel. Get speed, low battery, and GeoFence entry / exit alerts, all from the convenience of your phone. Find and recover your car using real-time GPS tracking.",
-    techStack: ["Kotlin", "Hilt", "Room", "Maps SDK", "Retrofit", "Gson"],
-    link: "https://play.google.com/store/apps/details?id=com.spireon.kahu&hl=en&gl=US",
-    image: "/images/lojack.webp"
-  },
-  {
-    name: "GoldStar",
-    description: "GoldStar goes beyond your typical GPS tracking system by offering a powerful collateral management solution featuring an intuitive, user-friendly interface that puts all your tracking data at just a click away.",
-    techStack: ["Kotlin", "Hilt", "Room", "Maps SDK", "Retrofit", "Gson"],
-    link: "https://play.google.com/store/apps/details?id=com.spireon.android.gsdealer",
-    image: "/images/goldstar.webp"
-  },
-  {
-    name: "Periscope",
-    description: "For fleet managers who need fleet visibility on-the-go, use FleetLocate Periscope, the mobile companion to FleetLocate by Spireon. Be the first to know where your drivers and vehicles are at all times. Understand when your drivers are speeding or moving assets without authorization.",
-    techStack: ["Kotlin", "Hilt", "Room", "Maps SDK", "Retrofit", "Gson"],
-    link: "https://play.google.com/store/apps/details?id=com.spireon.fleet",
-    image: "/images/periscope.webp"
-  }
-];
+import projects from "../data/projectData";
 
 export default function ProjectComponent() {
   return (
